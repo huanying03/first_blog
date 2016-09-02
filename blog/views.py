427e9b88d05bc1e198412d88a312ded6f0264b0f
lsq <lsq@idx069.(none)>
2016-09-02 10:35:31 +0800

    blog: ImportError(blog#1)

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound,HttpResponseRedirect,JsonResponse,FileResponse
+from django.template import loader
+from .models import blog
 
-# Create your views here.
+# 
 
-def hello(request):
-    return HttpResponse('<html>Hello world</html>')
+def showBlog(request,blogId):
+	t = loader.get.template('blog.html')
+	blog = Blog.objects.get(id=blogId)
+	context = {'blog':blog}
+	html = t.render(context)
+	return HttpResponse(html)
+
+def showBlogList(request):
+	t = loader.get_template('blog_list.html')
+	blogs=Blog.objects.all()
+	context = {'blogs':blogs}
+	html = t.render(context)
+	return HttpResponse(html)
+
+
